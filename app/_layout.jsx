d7c3a469b76ca4469d4e3fd3341c8aee706a2221
LisPOLSKA <jankuziok@gmail.com>
2024-11-01 22:23:31 +0100

    fix auth protext and blinking markers

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -10,34 +10,34 @@ import { auth } from "../firebase";
 import "../global.css"
 
 const _layout = () => {
-  const [isLogin, setIsLogin] = useState(null);
+    const [isLogin, setIsLogin] = useState(null);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setIsLogin(!!currentUser);
-    });
-    return unsubscribe;
-  }, []);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setIsLogin(!!currentUser);
+        });
+        return unsubscribe;
+    }, []);
 
-  if (isLogin === null) {
+    if (isLogin === null) {
+        return (
+            <View>
+                <ActivityIndicator size="large" color={primaryColor} />
+            </View>
+        );
+    }
+
     return (
-      <View>
-        <ActivityIndicator size="large" color={primaryColor} />
-      </View>
+        <AuthContextProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+                {isLogin ? (
+                    <Stack.Screen name="(tabs)" />
+                ) : (
+                    <Stack.Screen name="(auth)" />
+                )}
+            </Stack>
+        </AuthContextProvider>
     );
-  }
-
-  return (
-    <AuthContextProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        {isLogin ? (
-          <Stack.Screen name="(tabs)" />
-        ) : (
-          <Stack.Screen name="(auth)" />
-        )}
-      </Stack>
-    </AuthContextProvider>
-  );
 };
 
 export default _layout;
--- firebase.js
+++ firebase.js
@@ -4,13 +4,16 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const apiKey = process.env.EXPO_PUBLIC_FIREBASE_APIKEY
+const appId = process.env.EXPO_PUBLIC_FIREBASE_APPID
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  apiKey: apiKey,
   authDomain: "ecofy-c5818.firebaseapp.com",
   projectId: "ecofy-c5818",
   storageBucket: "ecofy-c5818.firebasestorage.app",
   messagingSenderId: "809034977854",
-  appId: process.env.REACT_APP_FIREBASE_APPID,
+  appId: appId,
   measurementId: "G-QL65760CJJ",
 };
 
